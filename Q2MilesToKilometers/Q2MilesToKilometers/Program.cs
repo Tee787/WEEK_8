904f57899c042e06d0f795e1d31afeb27a7f4c93
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-09-20 19:59:29 +1200

    first commit

---
+++ Q2MilesToKilometers/Q2MilesToKilometers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2MilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************************************************");
+            Console.WriteLine("***********************Convert Miles to Kilometers*******************");
+            Console.WriteLine("*********************************************************************");
+
+            double miles, kilometers;
+
+            Console.WriteLine("\nPlease enter miles: ");
+            miles = Convert.ToDouble(Console.ReadLine());
+
+            kilometers = miles * 1.6;
+
+            ConvertToKms(kilometers);
+            
+            //Console.WriteLine(kilometers + "kilometers");
+
+        }
+        public static void ConvertToKms(double kilometers)
+        {
+            Console.WriteLine(kilometers + "kilometers");
+        }
+    }
+}
---
+++ RadiusAndArea_Circle/RadiusAndArea_Circle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiusAndArea_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************************************************************************");
+            Console.WriteLine("*******************************Radius / Area_Circle******************************");
+            Console.WriteLine("*********************************************************************************");
+
+            Console.WriteLine("Calculator for Radius, Diameter, Circumference and Area of a circle.\n");    
+
+            double diameter, circumference, area;                                                       
+
+            Console.Write("\nEnter the radius: ");                                                      
+            int radius = Convert.ToInt32(Console.ReadLine());                                           
+            Console.WriteLine("\nThe radius is: {0}cm ", radius);                                       
+            Console.WriteLine("\nThe diameter is: {0}cm ", 2 * radius);
+            Circumference(int, radius);
+            //Console.WriteLine("\nThe circumference is: {0}cm ", Math.Round(2 * Math.PI * radius, 1));  
+            Console.WriteLine("\nThe area is: {0}cm ", Math.Round(Math.PI * (radius * radius), 1));
+        }
+
+        static void Circumference(int radius)
+        {
+            Console.WriteLine("\nThe circumference is: {0}cm ", Math.Round(2 * Math.PI * 2 * radius, 1));
+        }
+    }
+}
